28e584cab8a9625d3fcf2eb677baeffe20f15239
Dasha Stashkevich <stash.dashawork@gmail.com>
2024-12-11 19:00:55 +0300

    add comments for files

--- server.js
+++ server.js
@@ -4,51 +4,76 @@ const WebSocket = require('ws');
 const multer = require('multer');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const port = 8580;
+
+let dataFilesPath = './uploads/uploads.json';
+
 // Настройка хранения файлов с помощью multer
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-   const originalName = Buffer.from(file.originalname, 'latin1').toString('utf8')
-    cb(null, originalName); // сохраняем с оригинальным именем
-  }
+   destination: (req, file, cb) => {
+      
+      cb(null, 'uploads/');
+   },
+   filename: (req, file, cb) => {
+      const originalName = Buffer.from(file.originalname, 'latin1').toString('utf8')
+      cb(null, originalName); // сохраняем с оригинальным именем
+   }
 });
 
-const upload = multer({ storage: storage });
-
-// Middleware для поддержки CORS
+const upload = multer({ storage: storage })
+app.use('/', express.static(path.join(__dirname)));
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+
+const readFileData = () => {
+   if (fs.existsSync(dataFilesPath)) {
+     const data = fs.readFileSync(dataFilesPath);
+     return JSON.parse(data);
+   }
+   return [];
+ };
+
+ const writeFileData = (fileData) => {
+   fs.writeFileSync(dataFilesPath, JSON.stringify(fileData, null, 2));
+ };
 
 // Endpoint для загрузки файла
 app.post('/upload', upload.single('file'), (req, res) => {
-  const comment = req.body.comment;
-  console.log(`File uploaded: ${req.file.originalname} with comment: ${comment}`);
-  res.sendStatus(200);
+   const comment = req.body.comment;
+   const fileInfo = {
+      filename: req.file.originalname,
+      path: req.file.path,
+      comment: comment
+   }
+
+   const fileData = readFileData();
+   fileData.push(fileInfo);
+   writeFileData(fileData);
+ 
+   res.send('Файл загружен с комментарием: ' + comment);
+   
 });
 
 // Endpoint для получения списка загруженных файлов
 app.get('/files', (req, res) => {
-  const fs = require('fs');
-  const files = fs.readdirSync(path.join(__dirname, 'uploads'));
-  res.json(files);
+   const fileData = readFileData();
+  res.send(fileData);
 });
+
 
 // WebSocket для отслеживания прогресса
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
+   console.log('Client connected');
+   ws.on('close', () => {
+      console.log('Client disconnected');
+   });
 });
 
-// Слушаем на порту 3000
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+server.listen(port, () => {
+   console.log('Server is running on port', port);
 });
